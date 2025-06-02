c565bb819614d2e41b4661848c137c1051bff268
Eason0117 <158804964+Eason0117@users.noreply.github.com>
2025-06-02 12:25:33 +0800

    Update 17.user_delete.php

--- 17.user_delete.php
+++ 17.user_delete.php
@@ -7,13 +7,13 @@
     }
     else{   
         $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
-        $sql="delete from user where id='{$_GET["id"]}'";
-        #echo $sql;
+        $sql="delete from user where id='{$_GET["id"]}'";    // 從 user 資料表中刪除指定 id 的使用者資料
+        #echo $sql;    // 偵錯用，顯示 SQL 指令
         if (!mysqli_query($conn,$sql)){
             echo "使用者刪除錯誤";
         }else{
             echo "使用者刪除成功";
         }
-        echo "<meta http-equiv=REFRESH content='3, url=18.user.php'>";
+        echo "<meta http-equiv=REFRESH content='3, url=18.user.php'>";    // 自動重新導向到 user.php 頁面
     }
 ?>
