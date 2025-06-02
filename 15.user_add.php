271c47ddbee98c04521741f7d9fc3ed0ae468810
Eason0117 <158804964+Eason0117@users.noreply.github.com>
2025-06-02 12:14:38 +0800

    Update 15.user_add.php

--- 15.user_add.php
+++ 15.user_add.php
@@ -1,25 +1,35 @@
+15
 <?php
-
-error_reporting(0);
-session_start();
+error_reporting(0);  // 關閉錯誤訊息
+session_start();     // 啟用 Session
+// 檢查使用者是否已登入
 if (!$_SESSION["id"]) {
     echo "請登入帳號";
-    echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
-}
-else{    
+    // 3 秒後跳轉到登入頁面
+    echo "<meta http-equiv='REFRESH' content='3; url=2.login.html'>";
+} else {
+    // 建立資料庫連線
+    // mysqli_connect(主機, 使用者, 密碼, 資料庫名稱)
+    $conn = mysqli_connect("dbyfree.net", "immust", "immustimmust", "immust");
+    
+    // 檢查連線是否成功
+    if (!$conn) {
+        die("資料庫連線失敗: " . mysqli_connect_error());
+    }
 
-   #mysqli_connect() 建立資料庫連結
-   $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
-   #mysqli_query() 從資料庫查詢資料
-   #新增資料SQL命令：insert into 表格名稱(欄位1,欄位2) values(欄位1的值,欄位2的值)
-   $sql="insert into user(id,pwd) values('{$_POST['id']}', '{$_POST['pwd']}')";
-   #echo $sql;
-   if (!mysqli_query($conn, $sql)) {
-     echo "新增命令錯誤";
-   }
-   else{
-     echo "新增使用者成功，三秒鐘後回到網頁";
-     echo "<meta http-equiv=REFRESH content='3, url=18.user.php'>";
-   }
+    // 組成 SQL 新增語法
+    // 將表單送來的 id 與 pwd 寫入 user 表格中
+    $sql = "INSERT INTO user (id, pwd) VALUES ('{$_POST['id']}', '{$_POST['pwd']}')";
+
+    // 執行 SQL 新增語法
+    if (!mysqli_query($conn, $sql)) {
+        echo "新增命令錯誤：" . mysqli_error($conn);
+    } else {
+        echo "新增使用者成功，三秒鐘後回到網頁";
+        echo "<meta http-equiv='REFRESH' content='3; url=18.user.php'>";
+    }
+
+    // 關閉資料庫連線
+    mysqli_close($conn);
 }
 ?>
